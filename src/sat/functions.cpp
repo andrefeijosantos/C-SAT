ee73be941e5bdc75d9e198c416e378ba7c531f4d
alfeijo28 <andre.santos1@ufv.br>
2025-04-13 12:02:10 -0300

    feat: copying, removing and size-checking functions for DPLL algorithm.

--- src/sat/functions.cpp
+++ src/sat/functions.cpp
@@ -19,6 +19,11 @@ void add_clause(CNF& formula, Clause* clause) {
     formula.insert(clause);
 }
 
+// Removes a clause from a formula.
+void rmv_clause(CNF& formula, Clause* clause) {
+    formula.erase(clause);
+}
+
 // Checks if a clause is an unit clause.
 bool unit_clause(Clause* clause) {
     return clause->size() == 1;
@@ -32,4 +37,37 @@ bool empty_formula(CNF& formula) {
 // Checks if a literal is within a clause.
 Clause::iterator in_clause(Clause* clause, Literal literal) {
     return clause->find(literal);
+}
+
+// Checks if a clause is an contradiction (i.e., an empty clause).
+bool is_contradiction(Clause* clause) {
+    return clause->size() == 0;
+}
+
+// Checks if a contradiction (i.e., an empty clause) is within a formula.
+bool has_contradiction(CNF& formula) {
+    for(Clause* clause : formula)
+        if(is_contradiction(clause))
+            return true;
+    return false;
+}
+
+// Copies a clause to a new one.
+// Please, note the high computational cost.
+Clause* copy_clause(Clause* clause) {
+    Clause* copy = new Clause;
+    for(Literal l : *clause) 
+        add_literal(copy, l);
+    return copy;
+}
+
+// Copies a formula to a new one.
+// Please, note the high computational cost.
+CNF* copy_formula(CNF& formula) {
+    CNF *copy = new CNF;
+    for(Clause* clause : formula) {
+        Clause *copied_clause = copy_clause(clause);
+        add_clause(*copy, copied_clause);
+    }
+    return copy;
 }
--- src/sat/functions.h
+++ src/sat/functions.h
@@ -22,6 +22,11 @@ void add_literal(Clause*, Literal);
 // @param clause A pointer to the clause to be added.
 void add_clause(CNF&, Clause*);
 
+// Removes a clause from a formula.
+// @param formula The formula where the clause will be removed.
+// @param clause A pointer to the clause to be removed.
+void rmv_clause(CNF&, Clause*);
+
 // Checks if a clause is an unit clause.
 // @param clause A pointer to a clause.
 // @return true, if it's a unit clause; false, otherwise.
@@ -36,5 +41,27 @@ bool empty_formula(CNF&);
 // @param literal The literal to find.
 // @returns A iterator to the literal, if it was found; clause.end(), otherwise.
 Clause::iterator in_clause(Clause*, Literal);
+
+// Checks if a clause is an contradiction (i.e., an empty clause).
+// @param clause A pointer to the clause.
+// @returns True, if it's a contradiction; false, otherwise.
+bool is_contradiction(Clause*);
+
+// Checks if a contradiction (i.e., an empty clause) is within a formula.
+// @param formula The formula to be checked.
+// @returns True, if it has a contradiction; false, otherwise.
+bool has_contradiction(CNF&);
+
+// Copies a clause to a new one.
+// Please, note the high computational cost.
+// @param clause The clause to be copied.
+// @returns A pointer to the copied clause.
+Clause* copy_clause(Clause*);
+
+// Copies a formula to a new one.
+// Please, note the high computational cost.
+// @param formula The formula to be copied.
+// @returns A pointer to the copied formula.
+CNF* copy_formula(CNF&);
 
 #endif
